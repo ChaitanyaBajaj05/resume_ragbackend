90cf00c88b7d5eaebadfda457fe756dcc9c4dfb3
Chaitanya bajaj <chaitanyabajaj42@gmail.com>
2025-10-05 22:10:40 +0530

    change

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,18 +1,27 @@
 from django.urls import path
-from .views import RegisterView, ResumeUploadView, ResumeListView, ResumeDetailView, AskView, JobListView,JobCreateView, JobDetailView, JobMatchView
+from .views import (
+    RegisterView,
+    ResumeUploadView,
+    ResumeListView,
+    ResumeDetailView,
+    AskView,
+    JobListView,
+    JobCreateView,
+    JobDetailView,
+    JobMatchView
+)
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path("register", RegisterView.as_view()),
-    path("resumes", ResumeListView.as_view()),
-    path("resumes/upload", ResumeUploadView.as_view()),
-    path("resumes/<uuid:id>", ResumeDetailView.as_view()),
-    path("ask", AskView.as_view()),
+    path("register/", RegisterView.as_view()),
+    path("resumes/", ResumeListView.as_view()),
+    path("resumes/upload/", ResumeUploadView.as_view()),
+    path("resumes/<uuid:id>/", ResumeDetailView.as_view()),
+    path("ask/", AskView.as_view()),
 
-    # Distinct path for listing and creating jobs
-    path("jobs", JobCreateView.as_view(), name="job-create"),  # POST only
-    path("jobs/list", JobListView.as_view(), name="job-list"), # GET only
-
-    path("jobs/<uuid:id>", JobDetailView.as_view()),
-    path("jobs/<uuid:id>/match", JobMatchView.as_view()),
+    # Jobs
+    path("jobs/", JobCreateView.as_view(), name="job-create"),
+    path("jobs/list/", JobListView.as_view(), name="job-list"),
+    path("jobs/<uuid:id>/", JobDetailView.as_view()),
+    path("jobs/<uuid:id>/match/", JobMatchView.as_view()),
 ]
